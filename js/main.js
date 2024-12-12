b34fad54c5fb9ae366cc3123258b583d5f64e6bc
MagicThyme569 <dmaltabelezabarros@gmail.com>
2024-12-12 13:36:37 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('light')
+    body.classList.toggle('light')
+})
